0858ca139c89596a10710d8ee555c173b05364f4
Fabricio Trindade <fabrtrindade@gmail.com>
2024-04-29 17:05:47 -0300

    Adding a listener to check success/failure of a request to the server

--- app/src/main/java/com/devmasterteam/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/PersonRepository.kt
@@ -1,9 +1,9 @@
 package com.devmasterteam.tasks.service.repository
 
+import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PersonModel
 import com.devmasterteam.tasks.service.repository.remote.PersonService
 import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
-import kotlinx.coroutines.flow.callbackFlow
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,16 +12,23 @@ class PersonRepository {
 
     private val remote  = RetrofitClient.getRetrofitService(PersonService::class.java)
 
-    fun login(email: String, password: String) {
+    fun login(email: String, password: String, listener: APIListener<PersonModel>) {
 
         val call = remote.login(email, password)
         call.enqueue(object: Callback<PersonModel> { //Note: async call!!!
             override fun onResponse(call: Call<PersonModel>, response: Response<PersonModel>) {
-                val s = ""
+                if (response.code() == 200) {
+                    val s = ""
+                    response.body()?.let { listener.onSuccess(it) }
+                }
+                else {
+                    //TODO - Tratar Json
+                    listener.onFailure(response.errorBody()!!.string())
+                }
             }
 
             override fun onFailure(call: Call<PersonModel>, t: Throwable) {
-                val s = ""
+                listener.onFailure("Unexpected error")
             }
         })
     }
