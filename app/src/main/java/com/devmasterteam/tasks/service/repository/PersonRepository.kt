7152b5db5590fa734df7dffd4dd7f0d9a6916aec
Fabricio Trindade <fabrtrindade@gmail.com>
2024-04-29 17:46:14 -0300

    Finalizing the PersonRepository repository

--- app/src/main/java/com/devmasterteam/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/PersonRepository.kt
@@ -1,36 +1,41 @@
 package com.devmasterteam.tasks.service.repository
 
+import android.content.Context
+import com.devmasterteam.tasks.R
+import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PersonModel
 import com.devmasterteam.tasks.service.repository.remote.PersonService
 import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
+import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PersonRepository {
+class PersonRepository(val context: Context) {
 
-    private val remote  = RetrofitClient.getRetrofitService(PersonService::class.java)
+    private val remote = RetrofitClient.getRetrofitService(PersonService::class.java)
 
     fun login(email: String, password: String, listener: APIListener<PersonModel>) {
 
         val call = remote.login(email, password)
-        call.enqueue(object: Callback<PersonModel> { //Note: async call!!!
+        call.enqueue(object : Callback<PersonModel> { //Note: async call!!!
             override fun onResponse(call: Call<PersonModel>, response: Response<PersonModel>) {
-                if (response.code() == 200) {
-                    val s = ""
+                if (response.code() == TaskConstants.HTTP.SUCCESS) {
                     response.body()?.let { listener.onSuccess(it) }
-                }
-                else {
-                    //TODO - Tratar Json
-                    listener.onFailure(response.errorBody()!!.string())
+                } else {
+                    listener.onFailure(failResponse(response.errorBody()!!.string()))
                 }
             }
 
             override fun onFailure(call: Call<PersonModel>, t: Throwable) {
-                listener.onFailure("Unexpected error")
+                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
             }
         })
+    }
+
+    private fun failResponse(str: String): String {
+        return Gson().fromJson(str, String::class.java)
     }
 
 }
