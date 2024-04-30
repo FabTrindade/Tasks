5e4b8ab787d9e7840f3847284ac3988068177733
Fabricio Trindade <fabrtrindade@gmail.com>
2024-04-30 14:27:32 -0300

    <LiveData> (observe): Initial Login Validation

--- app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
@@ -1,7 +1,10 @@
 package com.devmasterteam.tasks.viewmodel
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PersonModel
 import com.devmasterteam.tasks.service.repository.PersonRepository
@@ -10,15 +13,23 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     private val personRepository = PersonRepository(application.applicationContext)
 
+    private val _onSuccess = MutableLiveData<PersonModel>()
+    val onSuccess: LiveData<PersonModel> = _onSuccess
+
+    private val _onFailure = MutableLiveData<String>()
+    val onFailure: LiveData<String> = _onFailure
+
     /**
      * Faz login usando API
      */
     fun doLogin(email: String, password: String) {
         personRepository.login(email, password, object : APIListener<PersonModel> {
             override fun onSuccess(response: PersonModel) {
+                _onSuccess.value = response
             }
 
             override fun onFailure(message: String) {
+                _onFailure.value = message
             }
         })
     }
