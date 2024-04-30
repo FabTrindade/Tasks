9b756d8892b425956f5a1a6c9d0d194e328c4740
Fabricio Trindade <fabrtrindade@gmail.com>
2024-04-30 17:44:14 -0300

    SharedPreferences -> saving user security data

--- app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
@@ -5,10 +5,12 @@ import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PersonModel
 import com.devmasterteam.tasks.service.model.ValidationModel
 import com.devmasterteam.tasks.service.repository.PersonRepository
+import com.devmasterteam.tasks.service.repository.SecurityPreferences
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -16,7 +18,7 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     private val _onLogin = MutableLiveData<ValidationModel>()
     val onLogin: LiveData<ValidationModel> = _onLogin
-
+    private var securityPreferences = SecurityPreferences(application.applicationContext)
 
     /**
      * Faz login usando API
@@ -24,7 +26,9 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
     fun doLogin(email: String, password: String) {
         personRepository.login(email, password, object : APIListener<PersonModel> {
             override fun onSuccess(response: PersonModel) {
-
+                securityPreferences.store(TaskConstants.SHARED.PERSON_NAME, response.name)
+                securityPreferences.store(TaskConstants.SHARED.PERSON_KEY, response.personKey)
+                securityPreferences.store(TaskConstants.SHARED.TOKEN_KEY, response.token)
                 _onLogin.value = ValidationModel()
             }
 
