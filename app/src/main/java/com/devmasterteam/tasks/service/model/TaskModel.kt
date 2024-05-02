e08f982a2f08a65e337234ed90e344d8593396b1
Fabricio Trindade <fabrtrindade@gmail.com>
2024-05-02 13:43:56 -0300

    TaskRepository -> Creating a service for app tasks

--- app/src/main/java/com/devmasterteam/tasks/service/model/TaskModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/model/TaskModel.kt
@@ -1,4 +1,16 @@
 package com.devmasterteam.tasks.service.model
 
+import com.google.gson.annotations.SerializedName
+
 class TaskModel {
+    @SerializedName("Id")
+    val id: Int = 0
+    @SerializedName("PriorityId")
+    val priorityId: Int = 0
+    @SerializedName("Description")
+    val description: String = ""
+    @SerializedName("DueDate")
+    val dueDate: String = ""
+    @SerializedName("Complete")
+    val complete: Boolean = false
 }
