063b3fe2a1ced9611b7824d9dc0b8a9afba96db6
Fabricio Trindade <fabrtrindade@gmail.com>
2024-05-06 17:38:03 -0300

    Task registration -> Getting response from API

--- app/src/main/java/com/devmasterteam/tasks/viewmodel/TaskFormViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/TaskFormViewModel.kt
@@ -4,12 +4,15 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PriorityModel
 import com.devmasterteam.tasks.service.model.TaskModel
 import com.devmasterteam.tasks.service.repository.PriorityRepository
+import com.devmasterteam.tasks.service.repository.TaskRepository
 
 class TaskFormViewModel(application: Application) : AndroidViewModel(application) {
-    val priorityRepository = PriorityRepository(application.applicationContext)
+    private val priorityRepository = PriorityRepository(application.applicationContext)
+    private val taskRepository = TaskRepository(application.applicationContext)
 
     private var _priorityList = MutableLiveData<List<PriorityModel>>()
     var priorityList: LiveData<List<PriorityModel>> = _priorityList
@@ -19,7 +22,16 @@ class TaskFormViewModel(application: Application) : AndroidViewModel(application
     }
 
     fun save(task: TaskModel) {
+        taskRepository.create(task, object: APIListener<Boolean>{
+            override fun onSuccess(response: Boolean) {
+                val s = ""
+            }
 
+            override fun onFailure(message: String) {
+                val s = ""
+            }
+
+        })
     }
 
 
