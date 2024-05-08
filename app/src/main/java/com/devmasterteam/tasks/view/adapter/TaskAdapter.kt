740b6f20f3fc15422a0aa4ec08fd2a11522e98e5
Fabricio Trindade <fabrtrindade@gmail.com>
2024-05-08 18:09:21 -0300

    RecyclerView -> Adjustment of task listing

--- app/src/main/java/com/devmasterteam/tasks/view/adapter/TaskAdapter.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/adapter/TaskAdapter.kt
@@ -11,19 +11,7 @@ import com.devmasterteam.tasks.view.viewholder.TaskViewHolder
 class TaskAdapter : RecyclerView.Adapter<TaskViewHolder>() {
 
     private var listTasks: List<TaskModel> = arrayListOf()
-    private var listener = object: TaskListener{
-        override fun onListClick(id: Int) {
-        }
-
-        override fun onDeleteClick(id: Int) {
-        }
-
-        override fun onCompleteClick(id: Int) {
-        }
-
-        override fun onUndoClick(id: Int) {
-        }
-    }
+    private lateinit var listener:  TaskListener
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
@@ -46,8 +34,8 @@ class TaskAdapter : RecyclerView.Adapter<TaskViewHolder>() {
     }
 
 
-    /*fun attachListener(taskListener: TaskListener) {
+    fun attachListener(taskListener: TaskListener) {
         listener = taskListener
-    }*/
+    }
 
 }
